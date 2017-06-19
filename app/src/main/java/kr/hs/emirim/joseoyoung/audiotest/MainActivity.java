551768341c97f66194235b8b1b8f92c30dc892ef
joseoyoung <josy3465@naver.com>
2017-06-19 19:17:18 +0900

    #3 Event Programming

--- app/src/main/java/kr/hs/emirim/joseoyoung/audiotest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/joseoyoung/audiotest/MainActivity.java
@@ -1,13 +1,35 @@
 package kr.hs.emirim.joseoyoung.audiotest;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    Switch switchMusic;
+    MediaPlayer musicPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchMusic=(Switch)findViewById(R.id.switch_music);
+        musicPlayer=MediaPlayer.create(this,R.raw.I_Need_You);
+        switchMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(switchMusic.isChecked())
+                    musicPlayer.start();
+                else
+                    musicPlayer.pause();
+            }
+        });
+
     }
+    protected void onStop(){
+        super.onStop();
+        musicPlayer.stop();
+    }
+
 }
